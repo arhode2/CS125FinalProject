9e6b0c268b14a26fde8621e69a2dc3b1eff0fb28
andrewrhode <arhode2@illinois.edu>
2018-04-23 22:57:47 -0500

    Started out on API.

--- app/src/main/java/edu/illinois/cs/cs125/cs125finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/cs125finalproject/MainActivity.java
@@ -1,27 +1,51 @@
 package edu.illinois.cs.cs125.cs125finalproject;
-
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.util.Log;
-
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+//Used these for debugging. Might need them again eventually.
+
+/** <p>These are the packages we'll need for the API calls. Required some tweaking
+ * to build.gradle(Module:app) to get the files for the MainActivity to recognize the
+ * imports.
+ * Do not delete the unused ones. They will get used. </p>
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+    /** Context for the main activity, which should be the only one we need */
+    private static final String TAG = "CS125FinalProject.main";
+
+    /** Request queue for the API razzle dazzle**/
+    private static RequestQueue requestQueue;
+
+    /** Runs when the app is booted up */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /** Get the request queue ready. I'm taking this from Lab 12.
+         * I don't know it myself.
+         */
+        requestQueue = Volley.newRequestQueue(this);
+
         setContentView(R.layout.activity_main);
-        //ImageView nbaLogo = (ImageView)findViewById(R.id.imageView2);
-        //nbaLogo.setImageResource(R.drawable.nba_logo);
-        ImageView dRose = (ImageView)findViewById(R.id.imageView);
-        dRose.setImageResource(R.drawable.nba_logo);
+        ImageView nbaLogo = (ImageView)findViewById(R.id.ImageView_logo);
+        nbaLogo.setImageResource(R.drawable.nba_logo);
     }
-    private static final String TAG = "CS125FinalProject.main";
     public void sendMessage(View view) {
         TextInputEditText playerName = (TextInputEditText)findViewById(R.id.input_player);
         TextInputEditText season = (TextInputEditText)findViewById(R.id.input_season);
